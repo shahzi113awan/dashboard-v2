d9c7c22125c62ae008918be30148250ee7b56ee5
Shahzaib Manzoor <34761350+shahzi113awan@users.noreply.github.com>
2021-02-24 08:15:34 +0500

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/reactdb",
+//   mongoURI: "mongodb://localhost:27017/reactdb",
   // mongoURI:
   //   "mongodb+srv://shezi:12345@cluster0.uicnw.mongodb.net/onBoard?retryWrites=true&w=majority",
-  // mongoURI:
-  //   "mongodb+srv://tauqeer180:tauqeer180@cluster0.63fvo.mongodb.net/onBoard?retryWrites=true&w=majority",
+  mongoURI:
+    "mongodb+srv://tauqeer180:tauqeer180@cluster0.63fvo.mongodb.net/onBoard?retryWrites=true&w=majority",
 };
